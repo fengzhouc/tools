f14ad72337fdaf5bf7a74e29cf9fd1d362adff92
fengzhouc <748151866@qq.com>
2020-03-21 23:53:36 +0800

    add：help message

--- dirScan/lib/cmdline.py
+++ dirScan/lib/cmdline.py
@@ -11,7 +11,9 @@ import sys
 def parse_args():
     parser = argparse.ArgumentParser(description="dicts mast set in lib/config.py")
 
-    parser.add_argument("-f", help="Load new line delimited targets from urls, support excel and txt")
+    parser.add_argument("-f", help="Load targets from urls, support excel and txt; "
+                                   "eg: python dirScan.py -f xx.txt; "
+                                   "eg: python dirScan.py -f xx.xls -s 0,1,2")
 
     parser.add_argument("-s", help="excel sheet number, support more than one, eg: 0,1,2")
 
