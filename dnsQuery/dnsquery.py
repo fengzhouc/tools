57a96223b0b3ef0c747a89b5fde97c45bb200022
fengzhouc <748151866@qq.com>
2020-04-19 00:04:32 +0800

    add：multiprocessing

--- dnsQuery/dnsquery.py
+++ dnsQuery/dnsquery.py
@@ -1,8 +1,11 @@
 # encoding=utf-8
+
 import csv
+import multiprocessing
+import threading
+import time
 
 import dns
-import xlrd
 from dns import resolver
 from dns.exception import Timeout
 from dns.resolver import NXDOMAIN, NoAnswer
@@ -10,7 +13,7 @@ from dns.resolver import NXDOMAIN, NoAnswer
 from cmdline import parse_args
 
 
-def query(domain):
+def query(domain, rqueue, equeue):
     result = []
     cname = ""
     for t in ["CNAME", "A"]:
@@ -23,18 +26,29 @@ def query(domain):
                     cname = i
 
         except (NXDOMAIN, Timeout, NoAnswer) as e:
-            report([[domain, str(e)], ], "dna_error")
-    report(result, "dnsquery-all")
+            equeue.put([[domain, str(e)], ])
+        for r in result:
+            rqueue.put(r)
 
 
 # 写报告
-def report(data, filename):
-    file = "{}.csv".format(filename)
-
+def report():
+    global rqueue
+    global equeue
+    file = "{}.csv".format("dnsquery-all")
+    efile = "{}.csv".format("dnsquery-error")
+    time.sleep(5)
     with open(file, 'a', newline="\n") as f:
         w = csv.writer(f)
-        for _ in data:
-            w.writerow(_)
+        while not rqueue.empty():
+            w.writerow(rqueue.get())
+            time.sleep(0.5)
+    with open(efile, 'a', newline="\n") as f:
+        w = csv.writer(f)
+        while not equeue.empty():
+            w.writerow(equeue.get())
+            time.sleep(0.5)
+
 
 def get_urls():
     urls = []
@@ -48,6 +62,22 @@ def get_urls():
 
 
 if __name__ == '__main__':
-    domains = get_urls()
-    for domain in domains:
-        query(domain)
+    try:
+        domains = get_urls()
+        rqueue = multiprocessing.Manager().Queue()
+        equeue = multiprocessing.Manager().Queue()
+        start = time.time()
+        threading.Thread(target=report).start()
+        pool = multiprocessing.Pool()
+        for domain in domains:
+            pool.apply_async(func=query, args=(domain, rqueue, equeue))  # 维持执行的进程总数为processes，当一个进程执行完毕后会添加新的进程进去
+        print('Waiting for all subprocesses done...')
+        pool.close()
+        pool.join()  # 调用join之前，先调用close函数，否则会出错。执行完close后不会有新的进程加入到pool,join函数等待所有子进程结束
+        print("all done. tile: {}".format(time.time() - start))
+
+    except KeyboardInterrupt as e:
+        print('You aborted the scan.')
+        exit(-1)
+    except Exception as e:
+        print('\n[__main__.exception] %s %s' % (type(e), str(e)))
