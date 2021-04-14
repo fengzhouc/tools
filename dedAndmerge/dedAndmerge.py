d897b4a1aee80af035d7bc5b5a0198cff42e14df
user <123>
2021-04-14 17:20:27 +0800

    fix bug

--- dedAndmerge/dedAndmerge.py
+++ dedAndmerge/dedAndmerge.py
@@ -4,15 +4,27 @@ from cmdline import parse_args
 
 def ded():
     urls = []
+    ded_urls = []
     args = parse_args()
     f = args.f
+    # f = "domain.txt"
     if f.endswith("txt"):
         with open(f, encoding="utf-8") as file:
-            for url in file:
-                urls.append(url.strip())
-    with open("{}-new.txt".format(f), mode="w", newline="\n") as fie:
-        for url in list(set(urls)):
-            fie.write("{}\n".format(url))
+            with open("{}-new.txt".format(f), mode="w") as fie:
+                for url in file:
+                    for u in url.split(","):
+                        if u != "" and u.strip() not in urls:
+                            urls.append(u.strip())
+                            fie.write("{}\n".format(u.strip()))
+                        else:
+                            if u != "":
+                                ded_urls.append(u.strip())
+    for url in list(set(ded_urls)):
+        print("##ded data:")
+        print(url.strip())
+    # with open("{}-new.txt".format(f), mode="w", newline="\n") as fie:
+    #     for url in list(set(urls)):
+    #         fie.write("{}\n".format(url))
 
 
 if __name__ == '__main__':
